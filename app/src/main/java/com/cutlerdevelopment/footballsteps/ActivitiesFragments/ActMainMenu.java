d966f4529553ba59e6f2b2f62b1f957ce53d4ba0
Brendan <brendan.cutler1992@gmail.com>
2020-04-05 13:53:51 +0100

    Couple extra screens

---
+++ app/src/main/java/com/cutlerdevelopment/footballsteps/ActivitiesFragments/ActMainMenu.java
@@ -0,0 +1,16 @@
+package com.cutlerdevelopment.footballsteps.ActivitiesFragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cutlerdevelopment.footballsteps.R;
+
+public class ActMainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
